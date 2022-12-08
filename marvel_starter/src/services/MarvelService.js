56275a61e62aeac491b003e3552567f4cc637a8e
Kirill <kirill1991okt@gmail.com>
2022-12-08 21:42:48 +0300

    refactor: MarvelService.js

--- marvel_starter/src/services/MarvelService.js
+++ marvel_starter/src/services/MarvelService.js
@@ -1,6 +1,8 @@
 class MarvelServices {
-  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-  _apiKey = 'apikey=d5d684a06d58da256b5a00a75e3c5ddf';
+  constructor() {
+    this._apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    this._apiKey = 'apikey=d5d684a06d58da256b5a00a75e3c5ddf';
+  }
 
   getResource = async (url) => {
     const response = await fetch(url);
@@ -12,14 +14,39 @@ class MarvelServices {
     return await response.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+
+    return res.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  getCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    const transformDescription = (des) => {
+      if (des.length === 0) {
+        return 'No character data';
+      }
+
+      if (des.length > 210) {
+        return des.slice(0, 210) + '...';
+      }
+    };
+
+    return {
+      name: char.name,
+      description: transformDescription(char.description),
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
